b1f4de3246e024dbf93da36167d35f3663ab174d
Saber FATHALLAH <saberfathallah@ewm082.local>
2021-04-28 23:51:03 +0200

    dsdsds

--- backend/routes/User.js
+++ backend/routes/User.js
@@ -1,36 +1,46 @@
-const router = require('express').Router();
-let User = require('../models/User.model');
+const router = require("express").Router();
+let User = require("../models/User.model");
 
-router.route('/ajouter').post((req, res) => {
-    const Type = req.body.Type;
-    const email = req.body.email;
-    const password = req.body.password;
-    const typeOfCars = req.body.typeOfCars;
-    const FirstName = req.body.FirstName;
-    const LastName = req.body.LastName;
+router.route("/ajouter").post((req, res) => {
+  const Type = req.body.Type;
+  const email = req.body.email;
+  const password = req.body.password;
+  const typeOfCars = req.body.typeOfCars;
+  const FirstName = req.body.FirstName;
+  const LastName = req.body.LastName;
 
-    const newUser = new User ({
-        Type ,
-        FirstName ,
-        LastName,
-        email ,
-        password ,
-        typeOfCars 
-    });
-    // promise
-     newUser.save()
-        .then( (User) => res.json(  {User : User}   ) )
-        .catch(err => res.status(400).json('Error: ' + err));
+  const newUser = new User({
+    Type,
+    FirstName,
+    LastName,
+    email,
+    password,
+    typeOfCars,
+  });
+  // promise
+  newUser
+    .save()
+    .then((User) => res.json({ User: User }))
+    .catch((err) => res.status(400).json("Error: " + err));
 });
 
-router.route('/supprimer/:id').delete((req, res) => {
-    User.findByIdAndDelete(req.params.id)
-    .then( () => res.json('User supprimeé'))
-    .catch(err => res.status(400).json('Error: ' + err));
+router.route("/supprimer/:id").delete((req, res) => {
+  User.findByIdAndDelete(req.params.id)
+    .then(() => res.json("User supprimeé"))
+    .catch((err) => res.status(400).json("Error: " + err));
 });
 
-router.route('/supprimer').delete((req, res) => {
-    res.json('maghir params brabi thabet')
+router.route("/supprimer").delete((req, res) => {
+  res.json("maghir params brabi thabet");
+});
+
+router.route("/").get(async (req, res) => {
+  try {
+    const users = await User.find();
+    return res.json({ users });
+  } catch (err) {
+    res.status(400).json("Error: " + err);
+  }
 });
 
 module.exports = router;
--- src/components/Signup.js
+++ src/components/Signup.js
@@ -1,118 +1,85 @@
-import React from 'react';
-import "bootstrap/dist/css/bootstrap.min.css"
-import axios from 'axios';
+import React, { useState } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import axios from "axios";
 
-export default class Signup extends React.Component {
-  constructor() {
-    super()
-    this.state = {
-      Type: '',
-      FirstName: '',
-      LastName: '',
-      Email: '',
-      Password: '',
-    }
-    this.changeType = this.changeType.bind(this)
-    this.changeFirstName = this.changeFirstName.bind(this)
-    this.changeLastName = this.changeLastName.bind(this)
-    this.changeEmail = this.changeEmail.bind(this)
-    this.changePassword = this.changePassword.bind(this)
-  }
-  changeType(event) {
-    this.setState({
-      Type: event.target.value
-    })
-  }
-  changeFirstName(event) {
-    this.setState({
-      FirstName: event.target.value
-    })
-  }
-  changeLastName(event) {
-    this.setState({
-      LastName: event.target.value
-    })
-  }
-  changeEmail(event) {
-    this.setState({
-      Email: event.target.value
-    })
-  }
-  changePassword(event) {
-    this.setState({
-      Password: event.target.value
-    })
-  }
+const Signup = () => {
+  const [Type, setType] = useState("");
+  const [FirstName, setFirstName] = useState("");
+  const [LastName, setLastName] = useState("");
+  const [Email, setEmail] = useState("");
+  const [Password, setPassword] = useState("");
 
-  onSubmit(event) {
-    event.preventDefault()
+  const onSubmit = async (event) => {
+    event.preventDefault();
     const registered = {
-      Type: this.state.Type,
-      FirstName: this.state.FirstName,
-      LastName: this.state.LastName,
-      Email: this.state.Email,
-      Password: this.state.Password
-    }
-    axios.post('http://localhost:4000/User/Ajouter', registered)
-      .then(Response => console.log(Response.data))
+      Type,
+      FirstName,
+      LastName,
+      Email,
+      Password,
+    };
+    const user = await axios.post(
+      "http://localhost:5000/User/Ajouter",
+      registered
+    );
 
-    this.setState({
-      Type: '',
-      FirstName: '',
-      LastName: '',
-      Email: '',
-      Password: '',
-    })
-  }
+    setType("");
+    setFirstName("");
+    setLastName("");
+    setEmail("");
+    setPassword("");
+  };
 
-  render() {
-    return (
-      <div>
-        <div className='container'>
-          <div className='form-div'>
-            <form>
+  return (
+    <div>
+      <div className="container">
+        <div className="form-div">
+          <form onSubmit={onSubmit}>
             <input
-                type="text"
-                placeholder="Type"
-                onChange={this.changeType}
-                value={this.state.Type}
-                className='form-control form-group'
-              />
-              <input
-                type="text"
-                placeholder="FirstName"
-                onChange={this.changeFirstName}
-                value={this.state.FirstName}
-                className='form-control form-group'
-              />
-              <input
-                type="text"
-                placeholder="LastName"
-                onChange={this.changeLastName}
-                value={this.state.LastName}
-                className='form-control form-group'
-              />
-              <input
-                type="text"
-                placeholder="Email"
-                onChange={this.changeEmail}
-                value={this.state.Email}
-                className='form-control form-group'
-              />
-              <input
-                type="Password"
-                placeholder="Password"
-                onChange={this.changePassword}
-                value={this.state.Password}
-                className='form-control form-group'
-              />
-              <input type="submit" className='btn btn-danger btn-block' value="Submit" />
-
-            </form>
-          </div>
+              type="text"
+              placeholder="Type"
+              onChange={(event) => setType(event.target.value)}
+              value={Type}
+              className="form-control form-group"
+            />
+            <input
+              type="text"
+              placeholder="FirstName"
+              onChange={(event) => setFirstName(event.target.value)}
+              value={FirstName}
+              className="form-control form-group"
+            />
+            <input
+              type="text"
+              placeholder="LastName"
+              onChange={(event) => setLastName(event.target.value)}
+              value={LastName}
+              className="form-control form-group"
+            />
+            <input
+              type="text"
+              placeholder="Email"
+              onChange={(event) => setEmail(event.target.value)}
+              value={Email}
+              className="form-control form-group"
+            />
+            <input
+              type="Password"
+              placeholder="Password"
+              onChange={(event) => setPassword(event.target.value)}
+              value={Password}
+              className="form-control form-group"
+            />
+            <input
+              type="submit"
+              className="btn btn-danger btn-block"
+              value="Submit"
+            />
+          </form>
         </div>
       </div>
-    )
-  }
-}
+    </div>
+  );
+};
 
+export default Signup;
