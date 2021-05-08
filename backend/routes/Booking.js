7a51289e4e59a80f508de86773f7e9db96ae257c
zouhairmakhloufa <zouhairmakhloufa11@gmail.com>
2021-05-08 07:18:54 +0200

    new  commit

--- backend/routes/Booking.js
+++ backend/routes/Booking.js
@@ -1,8 +1,10 @@
 const router = require("express").Router();
 let Booking = require("../models/Booking.model");
 
+//JSON Web Token (JWT) est un standard ouvert  
+//Il permet l'échange sécurisé de jetons (tokens) entre plusieurs parties
+//Cette sécurité de l’échange se traduit par la vérification de l’intégrité des données 
 const jwt = require("jsonwebtoken");
-
 function getUserToken(token) {
   try {
     const decoded = jwt.verify(token, "jwt_secret");
@@ -30,48 +32,29 @@ router.route("/booking").post(async (req, res) => {
   const typeOfCars = req.body.typeOfCars;
   const driverId = req.body.driverId;
   const userId = getUserToken(req.body.token);
+  
   try {
     const nexBooking = await Booking.create({
       governorateAddressSource,
-      governorateAddressDestination,
-      carId: typeOfCars,
-      noteToDriver,
-      packaging,
-      service,
-      paymentMethode,
-      driverId,
-      largeur,
-      profondeur,
       addresSource,
+      governorateAddressDestination,
       addressDestination,
       poids,
       hauteur,
+      largeur,
+      profondeur,
+      carId: typeOfCars,
+      service,
+      packaging,
+      paymentMethode,
+      noteToDriver,
       userId,
+      driverId,
     });
 
     res.status(200).json({ nexBooking });
   } catch (err) {
     res.status(400).json("Error: " + err);
   }
-
-  // const newBooking = new Booking({
-  //   governorateAddressSource,
-  //   addresSource,
-  //   governorateAddressDestination,
-  //   addressDestination,
-  //   poids,
-  //   hauteur,
-  //   largeur,
-  //   profondeur,
-  //   service,
-  //   packaging,
-  //   paymentMethode,
-  //   noteToDriver,
-  // });
-
-  // newBooking
-  //   .save()
-  //   .then(() => res.json("Test!!!"))
-  //   .catch((err) => res.status(400).json("Error: " + err));
 });
 module.exports = router;
