09258a2cde7cc9643568ded2dce394a6ab34b272
Saber FATHALLAH <saberfathallah@ewm082.local>
2021-05-09 18:58:17 +0200

    mailer

--- backend/routes/Booking.js
+++ backend/routes/Booking.js
@@ -1,15 +1,17 @@
 const router = require("express").Router();
 let Booking = require("../models/Booking.model");
-
-//JSON Web Token (JWT) est un standard ouvert  
-//Il permet l'échange sécurisé de jetons (tokens) entre plusieurs parties
-//Cette sécurité de l’échange se traduit par la vérification de l’intégrité des données 
 const jwt = require("jsonwebtoken");
+var nodemailer = require("nodemailer");
+
+//JSON Web Token (JWT) est un standard ouvert
+//Il permet l'échange sécurisé de jetons (tokens) entre plusieurs parties
+//Cette sécurité de l’échange se traduit par la vérification de l’intégrité des données
+
 function getUserToken(token) {
   try {
     const decoded = jwt.verify(token, "jwt_secret");
 
-    return decoded.user;
+    return decoded.id;
   } catch (error) {
     return null;
   }
@@ -32,7 +34,7 @@ router.route("/booking").post(async (req, res) => {
   const typeOfCars = req.body.typeOfCars;
   const driverId = req.body.driverId;
   const userId = getUserToken(req.body.token);
-  
+
   try {
     const nexBooking = await Booking.create({
       governorateAddressSource,
@@ -50,6 +52,34 @@ router.route("/booking").post(async (req, res) => {
       noteToDriver,
       userId,
       driverId,
+    });
+
+    // 3andik id mte3 driver w id mte3 user a3ml finByed el kolwahed bch tjib mail mte3ou w ba3ed 7outoua louta
+    // a£9ra tw ta3ref win tjhoutou
+
+    const transporter = nodemailer.createTransport({
+      service: "gmail",
+      auth: {
+        user: "lena 7ot el email mte3ik el bch teb3eth bih kima zou@gmail.com",
+        pass:
+          "wlena trot mot de passe mte3 el mail fou9ani kima 256ddjsjdslskj",
+      },
+    });
+
+    const mailOptions = {
+      from:
+        "kifkif lena t7ot mail mte3ik el bch teb3eth bih kima zou@gmail.com",
+      to: "lena 7out mail eli bch teb3ethlou jareb ey mail bech testi",
+      subject: "lena thout eli theb objet mte3 mail ",
+      text: "lena el contenu mte3 mail rahou jek trajet ect ... juste testi",
+    };
+
+    transporter.sendMail(mailOptions, async (err, Response) => {
+      if (err) {
+        res.status(400).json({ error: err.message });
+      } else {
+        res.status(200).json({ nexBooking });
+      }
     });
 
     res.status(200).json({ nexBooking });
